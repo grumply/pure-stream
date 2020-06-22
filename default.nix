d832144dcd9ba400a7978c3b8bf4cd2193dfbc9e
Sean Hickman <sean@grumply.com>
2020-06-22 04:24:22 -0500

    Generlaize streamer and switch to props.

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
-{ mkDerivation, base, pure-elm, pure-intersection, stdenv }:
+{ mkDerivation, base, pure-default, pure-elm, pure-prop, stdenv }:
 mkDerivation {
   pname = "pure-stream";
   version = "0.8.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base pure-elm pure-intersection ];
+  libraryHaskellDepends = [ base pure-default pure-elm pure-prop ];
   homepage = "github.com/grumply/pure-stream";
   description = "Streaming views";
   license = stdenv.lib.licenses.bsd3;
--- src/Pure/Stream.hs
+++ src/Pure/Stream.hs
@@ -1,11 +1,14 @@
-{-# language ImplicitParams, ConstraintKinds, RankNTypes, OverloadedStrings #-}
-module Pure.Stream (Step,step,stream,folds,unfolds,cons,nil,suspended,Stream()) where
+{-# language ImplicitParams, ConstraintKinds, RankNTypes, OverloadedStrings, 
+      TemplateHaskell, RecordWildCards, ViewPatterns, PatternSynonyms,
+      MultiParamTypeClasses, TypeFamilies, FlexibleContexts #-}
+module Pure.Stream (module Pure.Stream, module Stream) where
 
 import Pure.Stream.Internal as Stream hiding (step)
 import qualified Pure.Stream.Internal as Stream
 
-import Pure.Elm hiding (Step,step)
-import qualified Pure.Intersection as I
+import Pure.Elm hiding (Step,step,features,children)
+import Pure.Data.Default
+import Pure.Data.Prop.TH
 
 import Data.Typeable
 import qualified Data.List as List
@@ -15,32 +18,46 @@ type Step = (?step :: IO ())
 step :: Step => IO ()
 step = ?step
 
-data Env state a = Env state (Step => state -> a -> (state,[View]))
+data Env state a = Env (Step => Streamer state a)
+data Model state a = Model (Streamer state a) (Stream IO a)
 
-newtype Model a = Model (Stream IO a)
+data Msg = Startup | Step
 
-data Msg = Step
+data Streamer state a = Streamer_
+  { producer :: Stream IO a
+  , features :: Features
+  , children :: [View]
+  , initial  :: state
+  , consumer :: state -> a -> (state,[View])
+  }
+
+instance Default state => Default (Streamer state a) where
+  def = Streamer_ nil def def def (\st _ -> (st,[]))
+
+deriveLocalComponent ''Streamer
 
 {-# INLINE stream #-}
-stream
-  :: (Typeable state, Typeable a)
-  => Stream IO a
-  -> state
-  -> (state -> a -> (state, [View]))
-  -> View
-stream = \s i f -> run (App [] [] [] (Model s) update view) (Env i f)
+stream :: (Typeable state, Typeable a) => (Step => Streamer state a) -> View
+stream s = run (App [Startup] [] [] (Model undef nil) update view) (Env s)
   where
-    update _ _ (Model s) = Model <$> Stream.step s
+    undef = Streamer_ undefined undefined undefined undefined undefined
 
-    view (Env i f) (Model s) = let ?step = command Step in
-      Div <||> 
-        [ Div <||> List.concat
-          ( Stream.folds (\_ -> []) (\_ _ -> []) 
-            (\a g s -> let (s',vs) = f s a in vs : g s') 
-            s i
+    update Startup (Env streamer) (Model _ _) = 
+      let ?step = command Step
+      in let s = streamer
+         in pure (Model s (producer s))
+
+    update _ _ (Model streamer s) = 
+      Model streamer <$> Stream.step s
+
+    view _ (Model Streamer_ {..} s) = 
+      -- Any case in which this shouldn't be a block-level component?
+      Div <| SetFeatures features |> 
+        ( (Div <||> List.concat
+            ( Stream.folds (\_ -> []) (\_ _ -> []) 
+              (\a g s -> let (s',vs) = consumer s a in vs : g s') 
+              s initial
+            )
           )
-        , I.Observer def
-          <| I.RootMargin "100px"
-           . I.Threshold [0]
-           . I.Action (\_ -> command Step)
-        ]
+        : children
+        )
